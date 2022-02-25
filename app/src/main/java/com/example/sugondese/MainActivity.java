cbe572d73b9fb19449efdd914c49a707a300be18
sarthak <mirjisarthak3@gmail.com>
2022-02-25 21:04:37 +0530

    Firebase done

--- app/src/main/java/com/example/sugondese/MainActivity.java
+++ app/src/main/java/com/example/sugondese/MainActivity.java
@@ -3,35 +3,24 @@ package com.example.sugondese;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
+        btn = findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginPage.class));
+            }
+        });
 
-        TextView signUpTV = findViewById(R.id.textView);
-
-        String text = "Not a user yet? Sign Up";
-        SpannableString spannableString = new SpannableString(text);
-        ForegroundColorSpan foregroundColorSpanYellow = new ForegroundColorSpan(Color.parseColor("#FF9700"));
-        spannableString.setSpan(foregroundColorSpanYellow, 16, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        signUpTV.setText(spannableString);
-
-        signUpTV.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(MainActivity.this, RegisterPage.class));
-                    }
-                }
-        );
     }
 }
