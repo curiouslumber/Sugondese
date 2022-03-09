44d3301cfce62571eb4a65c9640249c5a95a9f7b
noelpinto47 <noelpinto47@gmail.com>
2022-03-09 21:11:37 +0530

    dum

--- app/src/main/java/com/example/sugondese/HomePage.java
+++ app/src/main/java/com/example/sugondese/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.sugondese;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -18,6 +19,7 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_home_page);
 
         homepageIconBtn = findViewById(R.id.homeFragmentBtn);
