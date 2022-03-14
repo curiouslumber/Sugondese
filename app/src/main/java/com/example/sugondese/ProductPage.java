bd6ee0003885b6eaf9064b162dbccb83258f51f9
noelpinto47 <noelpinto47@gmail.com>
2022-03-14 23:42:04 +0530

    dum

--- app/src/main/java/com/example/sugondese/ProductPage.java
+++ app/src/main/java/com/example/sugondese/ProductPage.java
@@ -11,14 +11,16 @@ import android.widget.Toast;
 
 public class ProductPage extends AppCompatActivity {
 
-    String[] pizzeria = {"pizzeria", "pizzeria desc", "@drawable/pizza"};
-    String[] smokeHouseDeli = {"Smoke House Deli", "Smoke House Deli desc", "@drawable/pizza2"};
-    String[] mcDonald = {"Mc Donald's", "Mc Donald's", "@drawable/burger1"};
-    String[] burgerKing = {"Burger King", "Burger King description", "@drawable/burger2"};
-    String[] chinChinChu = {"Chin Chin Chu", "Chinese Desc", "@drawable/chinese2"};
-    String[] tapRestro = {"TAP Restro", "TAP Restro Desc", "@drawable/chinese3"};
-    String[] creamCentre = {"Cream Centre", "Cream Centre Desc", "@drawable/italian1"};
-    String[] newYorkerRestaurant = {"New Yorker Restaurant", "New Yorker Restaurant desc", "@drawable/italian3"};
+    String[] pizzeria = {"Pizzeria", "Established in 2015 with a concept to bring the authenticity of Italian wood-fired pizza to India.\n" +
+            "\n" +
+            "1441 Pizzeria a venture by Trofi Chain Factory Private Limited is the first initiative from this culinary art house. It will be a one of its kind, traditional Italian Wood Fired Pizzeria with all of its food being made solely in the wood oven. The concept and menu have been designed and created by the Italian Pizza Master chef Renato Viola who will also be training the chefs personally.", "@drawable/pizza"};
+    String[] smokeHouseDeli = {"Smoke House Deli", "We serve comfort food with a deep focus on ethically sourced local ingredients that are healthy, organic, and wholesome. Our produce comes straight from local farms across the country, so that you can enjoy the best of both flavour and health, handmade from scratch.", "@drawable/pizza2"};
+    String[] mcDonald = {"Mc Donald's", "Everything from Burgers, fries, shakes, wraps to coffee, coolers and amazing beverages ! Back in 1954, a man named Ray Kroc discovered a small burger restaurant in California, and wrote the first page of our history. From humble beginnings as a small restaurant, we're proud to have become one of the world's leading food service brands with more than 36,000 restaurants in more than 100 countries.", "@drawable/burger1"};
+    String[] burgerKing = {"Burger King", "Our Restaurants are known for serving high quality, great-tasting and affordable food. Founded in 1954, BURGER KING is the second largest fast food hamburger chain in  the world with about 18,000 restaurants operating in more than 100 countries and the US territories as of June 30, 2019. The original HOME OF THE WHOPPER, our commitment to ingredients, signature recipes and end-to-end online Burger King experience for millennials is what has defined our brand for more than 50 years.", "@drawable/burger2"};
+    String[] chinChinChu = {"Chin Chin Chu", "Comfortable Seating Area, Gastronomical Experience, Fancy Crowd, Family Place, Good Quality Food, Courteous Staff. Come try our Crispy Prawn Cheung Fun, Chili Tofu, Duck Salad, Balinese Curry, Watermelon Drink, Sushi Platter to name a few !", "@drawable/chinese2"};
+    String[] tapRestro = {"TAP Restro", "\"TAP '' Resto Bar at its best. Tap is an amalgamation of Lounge bar, sports bar, pub & a nightclub. A bold multi faceted venture by China Gate Group. Guests can choose from either unwinding, social gathering, partying hard or indulging themselves in any of featured gaming activities such as Dart machines, pool tables and more.  A new innovative 3 tier menu pricing concept makes it a wallet friendly affair. A haven for foodies showcasing a multi cuisine menu comprising of mexican, conti, indian & chinese offerings", "@drawable/chinese3"};
+    String[] creamCentre = {"Cream Centre", "Cream Centre is a veg bistro serving global cuisine interpreted through an Indian lens. Over the last 63 Years, we have been serving an ever-evolving menu of casual comfort food that is created by a team of people who are passionate about discovering new tastes and ingredients, while staying true to our Indian roots.", "@drawable/italian1"};
+    String[] newYorkerRestaurant = {"New Yorker Restaurant", "Perfect Location, Relaxed Atmosphere, Ample Seating Area, Decent Ambience, Family Restaurant. Burgers, pizzas, nachos & salads are served at this laid-back quick-serve restaurant.c", "@drawable/italian3"};
 
     TextView productTitle, productDes;
     ImageView productImg;
